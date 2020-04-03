d69a4aff17df896040935bcb42df92ed2c49cfed
天昱 郑 <1219979760@qq.com>
2020-04-03 14:31:12 +0800

    1

--- Class6-2/源.cpp
+++ Class6-2/源.cpp
@@ -4,5 +4,15 @@ using namespace cv;
 using namespace std;
 int main()
 {
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\a\\lena.jpg",1);
+	if (srcMat.empty())  return -1;
 
+	float angle = -20.0, scale = 1;
+	cv::Point2f center(srcMat.cols * 0.5, srcMat.rows * 0.5);
+	const cv::Mat affine_matrix = cv::getRotationMatrix2D(center, angle, scale);
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	cv::waitKey(0);
 }
